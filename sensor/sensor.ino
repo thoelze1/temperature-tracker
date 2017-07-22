77e2d82165cc0ebcf0da88ad70b6344fe944fff5
Tanner Hoelzel <thoelze1@binghamton.edu>
2017-07-23 01:35:11 +0200

    Remove comments

--- sensor/sensor.ino
+++ sensor/sensor.ino
@@ -1,25 +1,17 @@
 const int sensorPin = A0;
 
-void setup() {
+void setup()
+{
   Serial.begin(9600);
 }
 
-void loop() {
+void loop()
+{
   int sensorVal = analogRead(sensorPin);
-  //Serial.print("Sensor value: ");
-  //Serial.print(sensorVal);
   
   float voltage = (sensorVal/1024.0) * 5.0;
-  //Serial.print(", Volts: ");
-  //Serial.print(voltage);
-
   float celsius = (voltage - 0.5) * 100.0;
-  //Serial.print(", Celsius: ");
-  //Serial.print(celsius);
-
   float farenheit = (celsius * 9.0/5.0) + 32.0;
-  //Serial.print(", Farenheit: ");
-  //Serial.println(farenheit);
 
   Serial.println(farenheit);
 
